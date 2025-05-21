b480460860052bd6bc0f4e8c8b18d7e7dd381194
redmasterf0x <163235205+redmasterf0x@users.noreply.github.com>
2025-05-21 16:30:06 -0500

    Fdgdfg

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const supabase = createClient('https://YOUR_PROJECT.supabase.co', 'YOUR_ANON_KEY')
+const supabase = createClient('https://jcfebaoadebnmbucdpbc.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpjZmViYW9hZGVibm1idWNkcGJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4NTc4NDUsImV4cCI6MjA2MzQzMzg0NX0.YGi-q6Z8_G3sOS5aQH6rVcMwAbkC-j4Sxgk0Ym6OP1M')
 
 document.getElementById('login-btn').addEventListener('click', async () => {
   const { error } = await supabase.auth.signInWithOAuth({
